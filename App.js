c7639a61ccade29c4da09f4227635ea0c225a6da
Andrei Mandychev <amandychev@accengage.com>
2017-11-04 13:44:12 +0100

    Add bottons to send Accengage events

--- App.js
+++ App.js
@@ -11,23 +11,8 @@ import {
   Text,
   View
 } from 'react-native';
+import Button from 'react-native-button';
 import Acc from 'react-native-acc';
-
-var AccTracking = Acc.analytics.tracking;
-
-var papa = {
-  name: "Andrei",
-  age: 33
-};
-var maman = {
-    name: "Muriel",
-    age: 33
-};
-var nikita = {
-  name: "Nikita",
-  age: 1,
-  parents : [papa, maman]
-};
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -35,6 +20,39 @@ const instructions = Platform.select({
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
 });
+
+function trackEvent() {
+    var papa = {
+        name: "Andrei",
+        age: 33
+    };
+    var maman = {
+        name: "Muriel",
+        age: 33
+    };
+    var nikita = {
+        name: "Nikita",
+        age: 1,
+        parents : [papa, maman]
+    };
+    Acc.analytics.tracking.trackEvent(5001, nikita);
+}
+
+function trackLead() {
+    Acc.analytics.tracking.trackLead("mylabel", "123");
+}
+
+function trackCart() {
+    var item = {id: "07", label: "Reebook", category: "Shoes", currency: "EUR", price: 39.99, quantity: 3};
+    Acc.analytics.tracking.trackCart("02", item);
+}
+
+function trackPurchase() {
+    var item1 = {id: "01", label: "Label#1", category: "Cat#1", currency: "EUR", price: 9.99, quantity: 2};
+    var item2 = {id: "02", label: "Label#2", category: "Cat#1", currency: "EUR", price: 86.78, quantity: 1};
+    var item3 = {id: "03", label: "Label#3", category: "Cat#5", currency: "EUR", price: 40.09, quantity: 1};
+    Acc.analytics.tracking.trackPurchase("03", "EUR", 136.8, [item1, item2, item3]);
+}
 
 export default class App extends Component<{}> {
   render() {
@@ -49,7 +67,26 @@ export default class App extends Component<{}> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
-        <Text>{AccTracking.trackEvent(5001, nikita)}</Text>
+          <Button onPress={trackEvent}
+              containerStyle={styles.accbuttoncontainer}
+              style={styles.accbutton}>
+              Track Event
+          </Button>
+          <Button onPress={trackLead}
+              containerStyle={styles.accbuttoncontainer}
+              style={styles.accbutton}>
+              Track Lead
+          </Button>
+          <Button onPress={trackCart}
+              containerStyle={styles.accbuttoncontainer}
+              style={styles.accbutton}>
+              Track Cart
+          </Button>
+          <Button onPress={trackPurchase}
+              containerStyle={styles.accbuttoncontainer}
+              style={styles.accbutton}>
+              Track Purchase
+          </Button>
       </View>
     );
   }
@@ -71,5 +108,18 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  accbuttoncontainer: {
+      margin: 5,
+      padding: 10,
+      width: 200,
+      height: 45,
+      overflow: 'hidden',
+      borderRadius: 4,
+      backgroundColor: '#0000ff',
+  },
+  accbutton: {
+      fontSize: 20,
+      color: 'white',
   },
 });
