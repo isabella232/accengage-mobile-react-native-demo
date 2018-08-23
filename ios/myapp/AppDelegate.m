36badcac80b9113e576b829ad804ae3966e2874e
Bastien MATTHÄI <bastien_matthai1@hotmail.fr>
2018-08-23 15:42:22 +0200

    Finalize deeplink support for iOS
    -Defining  an URL Scheme (appReact)
    -Adding iOS callback for deeplinks support

--- ios/myapp/AppDelegate.m
+++ ios/myapp/AppDelegate.m
@@ -39,4 +39,10 @@
   return YES;
 }
 
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
 @end
